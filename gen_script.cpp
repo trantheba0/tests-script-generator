f5f209c8b2292dcb9b59ad9e11b13ffda7086f20
Bao Tran The <tran62the61ba03@gmail.com>
2025-02-20 14:48:39 +0700

    added subtask generator

--- gen_script.cpp
+++ gen_script.cpp
@@ -2,12 +2,23 @@
 #include "testlib.h"
 using namespace std;
 const string script_destination = "tests-script.txt";
+void addTest(string script);
 string formatWithRandom(string script, string suffix = "", long long range_l = 1, long long range_r = 1e18);
 string formatWithoutRandom(string script, string suffix = "");
 string getWaKiller(string script_pattern, string solution_1, string solution_2, int max_test = 1000, string script_suffix = "");
 string getTleKiller(string script_pattern, string solution, int max_test = 100, string script_suffix = "");
 bool compareResult(string file_1, string file_2, string inp = "");
+bool checkSubtask(vector<string> scripts, string solution, vector<string> wa_files);
+bool genSubtask(vector<pair<string, int> > scripts, string solution, vector<string> wa_files, 
+	string subtaskName = "", int max_test = 1000);
 
+int count_test = 0;
+void addTest(string script) {
+	++count_test;
+	// cerr << script << '\n';
+	cerr << "Completed test #" << count_test << '\n';
+	cout << script << '\n';
+}
 string formatWithRandom(string script, string suffix, long long range_l, long long range_r) {
 	return script + " " + to_string(rnd.next(range_l, range_r)) + " " + suffix + " > $";
 }
@@ -41,17 +52,49 @@ bool compareResult(string file_1, string file_2, string inp) {
 	f_2.close();
 	return 1;
 }
+bool checkSubtask(vector<string> scripts, string solution, vector<string> wa_files) {
+	vector<bool> failed(wa_files.size(), 0);
+	for (auto &script : scripts) {
+		system(script.c_str());
+		system(solution.c_str());
+		for (int i = 0; i < (int)wa_files.size(); ++i) {
+			if (failed[i]) continue;
+			string wa_file = wa_files[i];
+			system(wa_file.c_str());
+			bool ok = !compareResult(solution + ".out", wa_file + ".out");
+			if (ok) failed[i] = 1;
+		}
+	}
+	for (int i = 0; i < (int)wa_files.size(); ++i) {
+		if (!failed[i]) return 0;
+	}
+	return 1;
+}
+bool genSubtask(vector<pair<string, int> > scripts, string solution, vector<string> wa_files, 
+	string subtaskName, int max_test) {
+	while (max_test--) {
+		vector<string> cur;
+		for (auto &x : scripts) {
+			string script = x.first;
+			int num = x.second;
+			for (int i = 0; i < num; ++i) {
+				cur.push_back(formatWithRandom(script, subtaskName));
+			}
+		}
+		if (checkSubtask(cur, solution, wa_files)) {
+			for (auto &script : cur) {
+				addTest(script);
+			}
+			return 1;
+		}
+	}
+	cerr << "Can't generate subtask named " << subtaskName << " with " << max_test << " rounds of generating!\n";
+	return 0;
+}
 signed main(int argc, char *argv[]) {
 	registerGen(argc, argv, 1);
 	#ifdef LOCAL
 		freopen(script_destination.c_str(), "w", stdout);
 	#endif
-	int count_test = 0;
-	auto addTest = [&](string script) {
-		++count_test;
-		// cerr << script << '\n';
-		cerr << "Completed test #" << count_test << '\n';
-		cout << script << '\n';
-	};
 	
 }
